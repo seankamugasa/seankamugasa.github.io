a79614483ee019cc70e9509b6d4e96f3aa248ecb
Sean Kamugasa <kamugasasean@yahoo.com>
2022-08-22 14:22:15 -0400

    portfolio update

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,6 @@
+jQuery(document).ready(function() {
+	
+	"use strict";
+	// Your custom js code goes here.
 
-//
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrollingSpeed': 600,
-				'autoScrolling': false,
-				'css3': true,
-				'navigation': true,
-				'navigationPosition': 'right',
-			});
-		});
-
-// wow
-$(function()
-{
-    new WOW().init();
-    $(".rotate").textrotator();
-})
+});
